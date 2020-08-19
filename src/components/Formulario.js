74926c242628531aaec85dfb15b3948a6975c122
lmencos <lmencos@hotmail.com>
2020-08-18 19:51:33 -0500

    states de concepto y cantidad OK

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,27 @@
 import React, { useState } from 'react';
 
 const Formulario = () => {
+
+  const [ concepto, guardarConcepto ] = useState('');
+  const [ cantidad, guardarCantidad ] = useState(0);
+
+  //Cuando el usuario agregue un gasto "botón agregar gasto"
+  const agregarGasto = (e) => {
+    e.preventDefault();
+
+    // Validar captura
+
+    // Construir el gasto
+
+    // Pasar el gasto al componente principal App
+
+    // Resetear el formulario
+  }
+
   return (  
-    <form>
+    <form
+      onSubmit={agregarGasto}
+    >
       <h2 className="titulo1">Agrega tus gastos aquí:</h2>
       <div className="campo" >
         <label htmlFor="">Nombre del gasto</label>
@@ -10,6 +29,8 @@ const Formulario = () => {
             type="text"
             className="u-full-width"
             placeholder="Ej. trasporte"
+            value={concepto}
+            onChange={ e => guardarConcepto(e.target.value) }
           />
       </div>
       <div className="campo" >
@@ -18,13 +39,14 @@ const Formulario = () => {
             type="number"
             className="u-full-width"
             placeholder="Ej. 300"
+            value={cantidad}
+            onChange={ e => guardarCantidad( parseInt(e.target.value), 10) }
           />
       </div>
-      <input 
+      <button 
         type="text"
         className="button-primary u-full-width"
-        value="Agregar gasto"
-        />
+      >Agregar gasto</button>
     </form>
   );
 }
